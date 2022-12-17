11b4c4e8e7ca1bf7a284dc704415341c07168823
reyane remache <reyane.a.remache@gmail.com>
2022-12-17 13:41:12 +0100

    background color change

--- Blockversity/src/components/Welcome.tsx
+++ Blockversity/src/components/Welcome.tsx
@@ -85,20 +85,8 @@ const Welcome: React.FC = () => {
               </div>
             </div>
           </div>
-          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
-            {/* <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
-            <Input placeholder="Amount (FLOW)" name="amount" type="number" handleChange={handleChange} />
-            <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} /> */}
+          <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism"> 
 
-            <div className="h-[1px] w-full bg-gray-400 my-2" />
-
-                <button
-                  type="button"
-                //   onClick={handleSubmit}
-                  className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
-                >
-                  Send now
-                </button>
           </div>
         </div>
 
