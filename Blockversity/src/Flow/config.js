f4072c6911110cf45de1c89a6e95a3ec34300c6b
reyane remache <reyane.a.remache@gmail.com>
2022-12-26 19:47:00 +0100

    added placeholder logo

--- Blockversity/src/Flow/config.js
+++ Blockversity/src/Flow/config.js
@@ -1,13 +1,8 @@
 import { config } from "@onflow/fcl";
 
 config()
-<<<<<<< HEAD
   .put('app.detail.title', "Sample DAO")
-  .put('app.detail.icon', '../assets/flow-logo.png')
-=======
-  .put('app.detail.title', "BlockVersity")
-  //.put('app.detail.icon', 'https://www.blockversity.xyz/img/logo-1-1@1x.png')
->>>>>>> 10cf48f50083f869938c73f6a6df4ecf0e1bcf82
+  .put('app.detail.icon', 'https://clipground.com/images/placeholder-logo-6.png')
   .put('accessNode.api', 'https://rest-testnet.onflow.org')
   .put('discovery.wallet', 'https://fcl-discovery.onflow.org/testnet/authn');
   //.put('accessNode.api', 'https://rest-mainnet.onflow.org')
