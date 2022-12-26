f04e38b6ef8e8c887b5988d7d2f80a45db1c967e
reyane remache <reyane.a.remache@gmail.com>
2022-12-26 19:14:12 +0100

    wallet logo

--- Blockversity/src/Flow/config.js
+++ Blockversity/src/Flow/config.js
@@ -1,8 +1,8 @@
 import { config } from "@onflow/fcl";
 
 config()
-  .put('app.detail.title', "BlockVersity")
-  .put('app.detail.icon', 'https://www.blockversity.xyz/img/logo-1-1@1x.png')
+  .put('app.detail.title', "Sample DAO")
+  .put('app.detail.icon', '../assets/flow-logo.png')
   .put('accessNode.api', 'https://rest-testnet.onflow.org')
   .put('discovery.wallet', 'https://fcl-discovery.onflow.org/testnet/authn');
   //.put('accessNode.api', 'https://rest-mainnet.onflow.org')
