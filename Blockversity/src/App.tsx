5fafe54d29ea7bdd7bd2cdb636745552664c343a
reyane remache <reyane.a.remache@gmail.com>
2022-12-26 18:39:49 +0100

    fixed footer

--- Blockversity/src/App.tsx
+++ Blockversity/src/App.tsx
@@ -1,7 +1,7 @@
 import{Navbar, Welcome, Footer} from './components';
 
 const App: React.FC = () => (
-  <div className="gradient-bg-welcome">
+  <div className="min-h-screen">
     <div className="gradient-bg-welcome">
       <Navbar />
        <Welcome /> 
