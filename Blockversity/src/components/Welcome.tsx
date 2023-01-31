db5c25dd38f0ac5cab6478b658ec99b5b131d0d3
reyane remache <reyane.a.remache@gmail.com>
2023-01-31 15:02:56 +0100

    token creation and distribution

--- Blockversity/src/components/Welcome.tsx
+++ Blockversity/src/components/Welcome.tsx
@@ -4,14 +4,47 @@ import { useEffect, useState } from "react";
 
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
 
+interface InputProps {
+  placeholder: string;
+  name: string;
+  type: string;
+  handleChange: (event: React.ChangeEvent<HTMLInputElement>, name: string) => void;
+}
+
+const Input: React.FC<InputProps> = ({ placeholder, name, type, value, handleChange }) => (
+  <input
+    placeholder={placeholder}
+    type={type}
+    step="1"
+    value={value}
+    onChange={(e) => handleChange(e, name)}
+    className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+  />
+);
+
 const Welcome: React.FC = () => {
   //   const { currentAccount, connectWallet, handleChange, sendTransaction, formData, isLoading } = useContext<TransactionContext>(TransactionContext);
   const [addresses, setAddresses] = useState([]);
   const [toggle, setToggle] = useState(false);
+  const [formData, setFormData] = useState({
+    addressTo: "",
+    amount: "",
+
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>, name: string) => {
+    setFormData({ ...formData, [name]: e.target.value });
+  };
+
 
   const showAddresses = () => {
     setToggle(!toggle);
   }
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const { addressTo, amount  } = formData;
+
+  };
 
   useEffect(() => {
     getAllAddresses().then((addresses) => {
@@ -54,6 +87,23 @@ const Welcome: React.FC = () => {
                   ))}
                 </div>
               )}
+            </div>
+          </div>
+
+          <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+            <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center text-white blue-glassmorphism">
+            <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+            <Input placeholder="Amount tokens" name="amount" type="number" handleChange={handleChange} />
+            <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
+            
+            <button
+                type="button"
+                onClick={handleSubmit}
+                className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+              >
+              Send now
+            </button>
+              
             </div>
           </div>
 
