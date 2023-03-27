f5e1aedac21c88ede1f51af5e06cdb5c26255bc7
reyane remache <reyane.a.remache@gmail.com>
2023-03-27 12:28:22 +0200

    Updated Admin

--- Blockversity/src/components/Admin.tsx
+++ Blockversity/src/components/Admin.tsx
@@ -1,259 +1,74 @@
-/** @format */
-
-import React, { useState, useEffect } from "react";
-import { Link } from "react-router-dom";
-import {
-  getBVTBalance,
-  getFUSDBalance,
-  getIsSaleActive,
-  getPrice,
-  getPurchasers,
-  setupBVT,
-  deployICO,
-  depositBVT,
-  pause,
-  unPause,
-  withdrawBVT,
-  refund,
-  distribute,
-} from "../Flow/ICOActions";
-
-import tokenomics from "./Tokenomics";
-
-interface InputProps {
-  placeholder: string;
-  name: string;
-  type: string;
-  handleChange: (
-    event: React.ChangeEvent<HTMLInputElement>,
-    name: string
-  ) => void;
-}
+import React, { useState } from "react";
+import CreateToken from "./CreateToken";
+import SellToken from "./SellToken";
+import ManageToken from "./ManageToken";
+import ICO from "./ICO";
+import Tokennomics from "./Tokenomics";
+import PurchaserList from "./PurchaserList";
 
 const AdminDashboard: React.FC = () => {
-  const [addressDistribute, setAddressDistribute] = useState("");
-  const [addressRefund, setAddressRefund] = useState("");
-  const [allocation, setAllocation] = useState(0);
-  const [tokenName, setTokenName] = useState("");
-  const [tokenAddress, setTokenAddress] = useState("");
-  const [price, setPrice] = useState(0);
-  const [salePrice, setSalePrice] = useState(0);
-  const [tokenBalance, setTokenBalance] = useState(0);
-  const [fUSDBalance, setFUSDBalance] = useState(0);
-  const [tokenSalePaused, setTokenSalePaused] = useState(true);
-  const [depositAmount, setDepositAmount] = useState(0);
-  const [withdrawAmount, setWithdrawAmount] = useState(0);
-  const [toggle, setToggle] = useState(false);
-  const [addresses, setAddresses] = useState([]);
+  const [activeTab, setActiveTab] = useState("createToken");
 
-  useEffect(() => {
-    getBVTBalance().then((_balance) => setTokenBalance(_balance));
-    getFUSDBalance().then((_balance) => setFUSDBalance(_balance));
-    getIsSaleActive().then((boolean) => setTokenSalePaused(!boolean));
-    getPrice().then((price) => setSalePrice(price));
-    getPurchasers().then((addresses) => {
-      setAddresses(addresses);
-    });
-  }, []);
-
-  const showAddresses = () => {
-    setToggle(!toggle);
-  };
-
-  const handlePriceChange = (value: number) => {
-    setPrice(value);
-  };
-
-  const handleDeposit = async () => {
-    const response = await depositBVT(depositAmount.toFixed(1));
-    console.log(response);
-    // logic to deposit BVT and update BVT balance
-  };
-
-  const handleWithdraw = async () => {
-    const response = await withdrawBVT(withdrawAmount.toFixed(1));
-    console.log(response);
-    // logic to withdraw BVT and update BVT balance
-  };
-
-  const handlePauseTokenSale = () => {
-    if (tokenSalePaused) {
-      unPause();
-    } else {
-      pause();
-    }
-  };
-
-  const handlePurchasers = async () => {
-    const response = await getPurchasers();
-    console.log(response);
-  };
-
-  const handleLaunch = async () => {
-    const response = await deployICO(price.toFixed(1), tokenAddress, tokenName);
-    console.log(response);
-  };
-
-  const handleDistribute = async () => {
-    distribute(addressDistribute, allocation.toFixed(1));
-  };
-  const handleRefund = async () => {
-    refund(addressDistribute);
-  };
-  const handleAllocationChange = (value: number) => {
-    setAllocation(value);
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
   };
 
   return (
-    <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
-      <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center text-white blue-glassmorphism'>
-        <Link to='/'>
-          <button
-            type='button'
-            className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer'>
-            Back to Home
-          </button>
-        </Link>
-        <div className='bg-dark-blue-500  p-8'>
-          <p className='text-white text-xl mb-4'>Admin Dashboard</p>
-          <div className='flex mb-4'>
-            <div className='w-1/2'>
-              <div className='text-white mb-2'>Price ($ FUSD)</div>
-              <input
-                type='range'
-                min={0}
-                max={500}
-                value={price}
-                onChange={(e) => handlePriceChange(Number(e.target.value))}
-                className='w-full'
-              />
-
-              <input
-                type='text'
-                placeholder='Token Name'
-                className='w-full text-black mb-4'
-                value={tokenName}
-                onChange={(e) => setTokenName(e.target.value)}
-              />
-              <input
-                type='text'
-                placeholder='Token Address'
-                className='w-full text-black mb-4'
-                value={tokenAddress}
-                onChange={(e) => setTokenAddress(e.target.value)}
-              />
-              <button
-                className='border-pink border-4 rounded-full p-2'
-                onClick={() => handleLaunch()}>
-                Launch ICO
-              </button>
-            </div>
-            <div className='w-1/2 text-white'>{price} FUSD</div>
-          </div>
-
-          <div className='mb-4'>
-            <h2 className='text-center mb-4'>BVT Current Sale Price {salePrice}</h2>
-            <div className='text-white mb-2 text-center'>
-              BVT in ICO contract Balance
-            </div>
-            <div className='text-white text-center'>{tokenBalance}</div>
-          </div>
-          <div className='mb-4'>
-            <div className='text-white mb-2 text-center'>FUSD in ICO Balance</div>
-            <div className='text-white text-center'>{fUSDBalance}</div>
-          </div>
-          <div className='mb-4'>
-            <input
-              type='number'
-              value={depositAmount}
-              onChange={(e) => setDepositAmount(Number(e.target.value))}
-              placeholder='Amount to deposit'
-              className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
-            />
+    <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+      <div className="bg-white shadow rounded-lg overflow-hidden">
+        <div className="px-4 py-5 sm:p-6">
+          <h2 className="text-2xl font-bold text-gray-800 mb-4">Admin Dashboard</h2>
+          <div className="flex flex-wrap justify-center">
             <button
-              className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer'
-              onClick={handleDeposit}>
-              Deposit BVT
+              className={`btn btn-outline-secondary m-2 ${activeTab === "createToken" ? "btn-primary" : ""
+                }`}
+              onClick={() => handleTabClick("createToken")}
+            >
+              Create Token
+            </button>
+            <button
+              className={`btn btn-outline-secondary m-2 ${activeTab === "sellToken" ? "btn-primary" : ""
+                }`}
+              onClick={() => handleTabClick("sellToken")}
+            >
+              Sell Token
+            </button>
+            <button
+              className={`btn btn-outline-secondary m-2 ${activeTab === "manageToken" ? "btn-primary" : ""
+                }`}
+              onClick={() => handleTabClick("manageToken")}
+            >
+              Manage Token
+            </button>
+            <button
+              className={`btn btn-outline-secondary m-2 ${activeTab === "ico" ? "btn-primary" : ""
+                }`}
+              onClick={() => handleTabClick("ico")}
+            >
+              ICO
+            </button>
+            <button
+              className={`btn btn-outline-secondary m-2 ${activeTab === "tokennomics" ? "btn-primary" : ""
+                }`}
+              onClick={() => handleTabClick("tokennomics")}
+            >
+              Tokennomics
+            </button>
+            <button
+              className={`btn btn-outline-secondary m-2 ${activeTab === "purchaserList" ? "btn-primary" : ""
+                }`}
+              onClick={() => handleTabClick("purchaserList")}
+            >
+              Purchaser List
             </button>
           </div>
-          <div className='mb-4'>
-            <input
-              type='number'
-              value={withdrawAmount}
-              onChange={(e) => setWithdrawAmount(Number(e.target.value))}
-              placeholder='Amount to withdraw'
-              className='my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism'
-            />
-            <button
-              className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer'
-              onClick={handleWithdraw}>
-              Withdraw BVT
-            </button>
-          </div>
-          <div className='mb-4'>
-            <button
-              className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer'
-              onClick={handlePauseTokenSale}>
-              {tokenSalePaused ? "UnFreeze" : "Freeze"} Token Sale
-            </button>
-          </div>
-          <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
-            <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center text-white blue-glassmorphism'>
-              <input
-                placeholder='Address To'
-                name='addressTo'
-                value={addressDistribute}
-                onChange={(e) => setAddressDistribute(e.target.value)}
-                className='text-black'
-              />
-              <div className='text-white mb-2'>Allocation Percentage ($BVT)</div>
-              <input
-                type='range'
-                min={0}
-                max={100}
-                value={allocation}
-                onChange={(e) => handleAllocationChange(Number(e.target.value))}
-                className='w-full'
-              />
-              <div className='w-1/2 text-white'>{allocation}%</div>
-
-              <button
-                type='button'
-                onClick={handleDistribute}
-                className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer'>
-                Distribute
-              </button>
-            </div>
-          </div>
-          <div className='flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10'>
-            <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center text-white blue-glassmorphism'>
-              <input
-                placeholder='Address to Refund'
-                name='addressTo'
-                value={addressRefund}
-                onChange={(e) => setAddressRefund(e.target.value)}
-                className='text-black'
-              />
-              <button
-                type='button'
-                onClick={handleRefund}
-                className='text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer'>
-                Refund
-              </button>
-            </div>
-          </div>
-
-          <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center text-white blue-glassmorphism mt-8'>
-            <button onClick={() => showAddresses()}>Get List of Purchasers</button>
-            {toggle && (
-              <div>
-                {addresses.map((address) => (
-                  <div key={address}>
-                    <p>{address}</p>
-                  </div>
-                ))}
-              </div>
-            )}
+          <div className="mt-6">
+            {activeTab === "createToken" && <CreateToken />}
+            {activeTab === "sellToken" && <SellToken />}
+            {activeTab === "manageToken" && <ManageToken />}
+            {activeTab === "ico" && <ICO />}
+            {activeTab === "tokennomics" && <Tokennomics />}
+            {activeTab === "purchaserList" && <PurchaserList />}
           </div>
         </div>
       </div>
---
+++ Blockversity/src/components/PurchaserList.tsx
@@ -0,0 +1,103 @@
+/** @format */
+
+import React, { useState } from "react";
+import { format } from "date-fns";
+import { Link } from "react-router-dom";
+import { getProposals } from "../Flow/GovernanceActions";
+
+type Proposal = {
+    id: string;
+    title: string;
+    description: string;
+    start_date: string;
+    end_date: string;
+    tokens_required: string;
+    options: boolean;
+};
+
+const recentData: Proposal[] = [
+    {
+        id: "1",
+        title: "chicken nuggets",
+        description: "chicken nuggets",
+        start_date: "2021-05-17T03:24:00",
+        end_date: "2022-05-17T03:24:00",
+        tokens_required: "100",
+        options: true,
+    },
+    // ...
+];
+
+export default function ProposalList() {
+    const [showActiveProposals, setShowActiveProposals] = useState(true);
+    const proposalData = getProposals();
+
+    console.log(getProposals(), "Called in the frontend");
+    const proposals = showActiveProposals
+        ? recentData.filter((proposal) => new Date(proposal.end_date) >= new Date())
+        : recentData.filter((proposal) => new Date(proposal.end_date) < new Date());
+
+    return (
+        <div className='flex justify-center items-center'>
+            <div className='px-4 pt-3 pb-4 flex flex-col text-white'>
+                <div className='flex-1'>
+                    <div className='flex justify-center mb-3'>
+                        <button
+                            className={`mr-3 py-1 px-2 border-b-2 ${showActiveProposals
+                                ? "border-blue-500 font-semibold"
+                                : "border-transparent"
+                                }`}
+                            onClick={() => setShowActiveProposals(true)}>
+                            Active Proposals
+                        </button>
+                        <button
+                            className={`py-1 px-2 border-b-2 ${!showActiveProposals
+                                ? "border-blue-500 font-semibold"
+                                : "border-transparent"
+                                }`}
+                            onClick={() => setShowActiveProposals(false)}>
+                            Past Proposals
+                        </button>
+                    </div>
+                    <div>
+                        <table className='w-full' style={{ tableLayout: "fixed" }}>
+                            <thead className='border border-gray-200 rounded-full my-3 custom-thead'>
+                                <tr>
+                                    <th>ID</th>
+                                    <th>TITLE</th>
+                                    <th>DESCRIPTION</th>
+                                    <th>START DATE</th>
+                                    <th>END DATE</th>
+                                    <th>
+                                        TOKENS
+                                        <br />
+                                        REQUIRED
+                                    </th>
+                                    <th>OPTIONS</th>
+                                </tr>
+                            </thead>
+                            <tbody className='border border-gray-200 bg-slate-500 rounded-sm mt-3 text-center'>
+                                {proposals.map((proposal: Proposal) => (
+                                    <tr key={proposal.id}>
+                                        <td>#{proposal.id}</td>
+                                        <td>{proposal.title}</td>
+                                        <td>{proposal.description}</td>
+                                        <td>
+                                            {format(new Date(proposal.start_date), "dd MMM yyyy")}
+                                        </td>
+                                        <td>
+                                            {format(new Date(proposal.end_date), "dd MMM yyyy")}
+                                        </td>
+                                        <td>{proposal.tokens_required}</td>
+                                        <td>{proposal.options ? "Accept" : "Reject"}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+}
